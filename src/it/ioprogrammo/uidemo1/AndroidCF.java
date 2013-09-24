f038aa15df2e873f0b4ccf1dc9f9494ca7d435d3
Davide Pastore <pasdavide@gmail.com>
2013-09-24 19:47:02 +0200

    Aggiunta una label.

--- src/it/ioprogrammo/uidemo1/AndroidCF.java
+++ src/it/ioprogrammo/uidemo1/AndroidCF.java
@@ -3,6 +3,8 @@ package it.ioprogrammo.uidemo1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class AndroidCF extends Activity {
 
@@ -17,6 +19,20 @@ public class AndroidCF extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	
+	/**
+	 * Reset the value of the fields
+	 * @param v
+	 */
+	public void onBtnClicked(View v){
+		EditText editTextName = (EditText) findViewById(R.id.editTextNome);
+		EditText editTextCognome = (EditText) findViewById(R.id.editTextCognome);
+		EditText editTextComune = (EditText) findViewById(R.id.editTextComune);
+		editTextName.setText("");
+		editTextCognome.setText("");
+		editTextComune.setText("");
 	}
 
 }
