ff249ed16f6f2f487f785bd0f9e064f128c3097b
Davide Pastore <d.pastore@logispin.com>
2020-09-11 16:15:02 +0200

    Add first test to calculate the Codice Fiscale

---
+++ app/src/test/java/it/androidcf/codicefiscale/CodiceFiscaleTest.java
@@ -0,0 +1,42 @@
+package it.androidcf.codicefiscale;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import it.androidcf.database.AndroidCFDB;
+
+@RunWith(RobolectricTestRunner.class)
+public class CodiceFiscaleTest {
+
+    Context instrumentationContext;
+
+    @Before
+    public void setup() {
+        instrumentationContext = InstrumentationRegistry.getInstrumentation().getContext();
+    }
+
+    private AndroidCFDB getAndroidCFDB() {
+        return new AndroidCFDB(instrumentationContext);
+    }
+
+    @Test
+    public void testCalcolaCodiceFiscale() throws Exception {
+        String nome = "Mario";
+        String cognome = "Rossi";
+        int giorno = 10;
+        int mese = 12;
+        int anno = 1985;
+        String sesso = "M";
+        String comuneDiNascita = "San Giuliano Terme";
+        CodiceFiscale codiceFiscale = new CodiceFiscale(nome, cognome, giorno, mese, anno, sesso, comuneDiNascita, getAndroidCFDB());
+        String calculated = codiceFiscale.calcola();
+        Assert.assertEquals("RSSMRA85T10A562S", calculated);
+    }
+}
